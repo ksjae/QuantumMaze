b6ea49832a10aac32a2f6153aed8c24fe22334a5
Seungjae Kim <ksjae0705@hotmail.com>
2021-02-18 12:44:07 +0900

    Pygame basic maze implementation

--- main.py
+++ main.py
@@ -3,43 +3,89 @@
 '''
 
 # Import tools
-import turtle
+from pygame.locals import *
+import pygame
 import qmaze
+ 
+class Player:
+    x = 44
+    y = 44
+    speed = 1
+ 
+    def moveRight(self):
+        self.x = self.x + self.speed
+ 
+    def moveLeft(self):
+        self.x = self.x - self.speed
+ 
+    def moveUp(self):
+        self.y = self.y - self.speed
+ 
+    def moveDown(self):
+        self.y = self.y + self.speed
 
-# Initialize GUI Window
-tScreen = turtle.Screen()
-tScreen.setup(width=500, height=450, startx=0, starty=0)
-turtle.title("QuMa - A Quantum Powered Maze")
-t = turtle.Turtle()
-t.fillcolor("red") # Set color of the cursor aka TURTLE
+# Initialization code
+windowWidth = 800
+windowHeight = 600
 
-# Moving turtle
-t.right(90)
-t.forward(100)
-
-# Warp turtle back to home when maze draw is done
-t.home()
-
-# Predefined turtle motion function
-def moveTurtleFoward():
-    t.forward(50)
-
-def endGame():
-    print("Click to exit")
-    tScreen.exitonclick()
-
-
-# Call func when key pressed
-tScreen.listen()
-tScreen.onkeypress(moveTurtleFoward, 'Right') 
-tScreen.onkeypress(endGame, 'Escape') 
-
-
-'''
-Gameplay
-'''
+running = False
+display_surf = None
+image_surf = None
+block_surf = None
+player = Player()
 maze = qmaze.Maze()
 
+def init():
+    pygame.init()
+    display_surf = pygame.display.set_mode((windowWidth,windowHeight), pygame.HWSURFACE)
+    
+    pygame.display.set_caption('Pygame pythonspot.com example')
+    running = True
+    image_surf = pygame.image.load("player.png").convert()
+    block_surf = pygame.image.load("block.png").convert()
 
-# When game is done
-tScreen.mainloop()
+
+
+# Game run code
+
+def on_event(event):
+    if event.type == QUIT:
+        running = False
+
+def on_loop():
+    pass
+
+def render():
+    display_surf.fill((0,0,0))
+    display_surf.blit(image_surf,(player.x,player.y))
+    maze.draw(display_surf, block_surf)
+    pygame.display.flip()
+
+def cleanup():
+    pygame.quit()
+
+
+init()
+
+while(running):
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+    
+    if (keys[K_RIGHT]):
+        player.moveRight()
+
+    if (keys[K_LEFT]):
+        player.moveLeft()
+
+    if (keys[K_UP]):
+        player.moveUp()
+
+    if (keys[K_DOWN]):
+        player.moveDown()
+
+    if (keys[K_ESCAPE]):
+        running = False
+
+    on_loop()
+    render()
+cleanup()
