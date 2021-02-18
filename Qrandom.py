8acd3a6503078b5bbfa5aabe00299573bb8b60ff
Seungbin Oh <sboh1214@gmail.com>
2021-02-18 17:39:17 +0900

    Fix Qrandom

--- Qrandom.py
+++ Qrandom.py
@@ -1,6 +1,7 @@
-import numpy as np
 from qiskit import(
     QuantumCircuit,
+    QuantumRegister,
+    ClassicalRegister,
     execute,
     Aer)
 simulator = Aer.get_backend('qasm_simulator')
@@ -14,12 +15,16 @@ def qrandom():
     circuit.h(qreg_q[0])
     circuit.measure(qreg_q[0], creg_c[0])
     
-    job=qiskit.execute(circuit,simulator,shots=1)
-    result=job.result()
-    counts=result.get_counts(circuit)
+    job = execute(circuit,simulator,shots=1)
+    result = job.result()
+    counts = result.get_counts(circuit)
 
     lAnswer = [(k[-1],v) for k,v in counts.items()]
-    lAnswer.sort(key = lambda x x[1], reverse=True)
+    lAnswer.sort(key = lambda x: x[1], reverse=True)
     Y = []
-    for k, v in lAnswer Y.append( [ int(c) for c in k ] )
+    for k, v in lAnswer: 
+        Y.append( [ int(c) for c in k ] )
     return Y[0][0]
+
+if __name__=="__main__":
+    print(qrandom())
